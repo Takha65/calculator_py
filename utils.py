2572ab5a5f228dd9a3cfa7ce12d36381a2771021
Takha65 <mountahambackendoye@gmail.com>
2025-03-31 14:28:51 -0400

    TICKET-77: ajout d'une fonction de calcul de puissance

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np 
+import math 
 def add(a, b):
     """
     Fonction qui additionne deux nombres.
@@ -6,7 +8,14 @@ def add(a, b):
 
 def multipli(a, b) : 
      """
-     Fonction qui multiplie deux fonctions 
+     Fonction qui multiplie deux fonctions. 
      
      """
      return a*b 
+
+def calculFactorial(a) : 
+     return math.factorial(a)
+
+def calcul_power(base, exponent):
+    """Élève 'base' à la puissance 'exponent'."""
+    return np.power(base, exponent)
